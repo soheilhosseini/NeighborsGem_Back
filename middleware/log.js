28d28977b02b24ccb6bb9b8d78ee021827a221d4
@s.hossein <s.hossein@rahkar.co>
2025-03-16 19:58:01 +0330

    init

--- middleware/log.js
+++
@@ -1,8 +0,0 @@
-const logEvents = require("/utils/logEvents.js");
-const log = (req, res, next) => {
-  logEvents(`${req.method}\t ${req.headers.origin}\t ${req.url}`, "reqLog.txt");
-  console.log(`${req.method} ${req.path}`);
-  next();
-};
-
-module.exports = log;
