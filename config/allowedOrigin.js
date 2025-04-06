fd7917725c42049f8808a6819adde0a2a99b1162
@s.hossein <s.hossein@rahkar.co>
2025-04-06 16:36:51 +0330

    1

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,8 +1,10 @@
 const allowedOrigins = [
-  "https://www.google.com",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
+  "http://localhost:3000",
   "https://localhost:3000",
+  "http://46.249.102.84:3500",
+  "https://46.249.102.84:3500",
 ];
 
 module.exports = allowedOrigins;
