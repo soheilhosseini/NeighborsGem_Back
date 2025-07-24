2030ad5026ceb936cb11059b3c638dc606ce761e
@s.hossein <s.hossein@rahkar.co>
2025-07-25 00:42:39 +0330

    1

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,5 +1,5 @@
 import admin, { ServiceAccount } from "firebase-admin";
-import serviceAccount from "./nesgem-1659d-firebase-adminsdk-fbsvc-b72dadc7ab.json";
+import serviceAccount from "./nesgem-1659d-firebase-adminsdk-fbsvc-c0693bbb47.json";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as ServiceAccount),
